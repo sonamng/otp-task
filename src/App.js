ba529ed46642499190e2790a3c71ff9383be339e
Sonam Kumari <sonam20@navgurukul.org>
2022-10-13 21:46:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+class Otpinput extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { value: '', otp1: "", otp2: "", otp3: "", otp4: "", otp5: "" };
+		console.log(this.state)
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	handleChange(value1, event) {
+		this.setState({ [value1]: event.target.value });
+		
+	}
+
+	handleSubmit(event) {
+		const data = new FormData(event.target);
+		event.preventDefault();
+	}
+
+	inputfocus = (elmnt) => {
+		if (elmnt.key === "Delete" || elmnt.key === "Backspace") {
+			const next = elmnt.target.tabIndex - 2;
+			if (next > -1) {
+				elmnt.target.form.elements[next].focus()
+				console.log("Backspace");
+			}
+		}
+
+		else {
+			console.log("next");
+
+			const next = elmnt.target.tabIndex;
+			if (next <5) {
+				elmnt.target.form.elements[next].focus()
+			}
+		}
+	}
+	
+	render() {
+		return (
+			<>
+			<div className="Navbar"></div>
+			<div className="div">
+				<form onSubmit={this.handleSubmit}>
+					<div className="otpContainer">
+							<input
+								name="otp1"
+								type="text"
+								className="otpInput"
+								value={this.state.otp1}
+								onKeyPress={this.keyPressed}
+								onChange={e => this.handleChange("otp1", e)}
+								tabIndex="1" maxLength="1" onKeyUp={e => this.inputfocus(e)}
+							/>
+
+							<input
+								name="otp2"
+								type="text"
+								className="otpInput"
+								value={this.state.otp2}
+								onChange={e => this.handleChange("otp2", e)}
+								tabIndex="2" maxLength="1" onKeyUp={e => this.inputfocus(e)}
+
+							/>
+
+
+							<input
+								name="otp3"
+								type="text"
+								className="otpInput"
+								value={this.state.otp3}
+								onChange={e => this.handleChange("otp3", e)}
+								tabIndex="3" maxLength="1" onKeyUp={e => this.inputfocus(e)}
+
+							/>
+							<input
+								name="otp4"
+								type="text"
+								className="otpInput"
+								value={this.state.otp4}
+								onChange={e => this.handleChange("otp4", e)}
+								tabIndex="4" maxLength="1" onKeyUp={e => this.inputfocus(e)}
+							/>
+
+							<input
+								name="otp5"
+								type="text"
+								className="otpInput"
+								value={this.state.otp5}
+								onChange={e => this.handleChange("otp5", e)}
+								tabIndex="5" maxLength="1" onKeyUp={e => this.inputfocus(e)}
+							/>
+
+
+							<input
+								name="otp6"
+								type="text"
+								className="otpInput"
+								value={this.state.otp5}
+								onChange={e => this.handleChange("otp6", e)}
+								tabIndex="6" maxLength="1" onKeyUp={e => this.inputfocus(e)}
+							/>
+
+						<button className="getotp" onClick={() =>(this.state.value) }>Get OTP</button>
+
+					</div>
+				</form>
+				<button className="delete" >Clear</button>
+
+				</div>
+			</>
+
+		);
+	}
 }
 
-export default App;
+
+export default Otpinput;
+
